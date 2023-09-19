58c2086bf5f60070892233b321815a29bfb83547
amangusss <120551474+amangusss@users.noreply.github.com>
2023-09-20 03:43:33 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const link1 = document.getElementById('linked');
+
+link1.addEventListener('click', (event) => {
+  event.preventDefault();
+  window.open('https://www.linkedin.com/in/aman-nazarkulov/'); 
+});
+
+const link2 = document.getElementById('telega');
+
+link2.addEventListener('click', (event) => {
+  event.preventDefault();
+  window.open('https://www.t.me/kindpo4antek'); 
+});
+
+const link3 = document.getElementById('insta');
+
+link3.addEventListener('click', (event) => {
+  event.preventDefault();
+  window.open('https://www.instagram.com/kindpo4antek/');
+});
